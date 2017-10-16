bad0da665058ec23308ce806dde2296f79b794b6
Nathan <nathan@gdquest.com>
2017-10-16 14:29:52 +0200

    Prepare all 3 UI demos with start/ and end/ versions

---
+++ demos/split/ui_code_life_bar/start/scripts/GUI.gd
@@ -0,0 +1,8 @@
+extends MarginContainer
+
+onready var Number = $Bars/LifeBar/Count/Background/Number
+onready var Bar = $Bars/LifeBar/TextureProgress
+onready var Tween = $Tween
+
+func _ready():
+	pass
